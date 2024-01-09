e6da62b4da4718ad88e5d20eb406976600a47ac4
lojain <lojainnasr039@gmail.com>
2024-01-09 12:48:25 +0300

    update

--- script.js
+++ script.js
@@ -8,6 +8,42 @@ function checkInput(){
         span.innerHTML = "";
     }
 }
-function validateInput(){
-    let day
+function validate() {
+    const day = document.getElementById('day').value;
+    const month = document.getElementById('month').value;
+    const year = document.getElementById('year').value;
+
+    let valid = true;
+
+    if(day < 1 || day > 31) {
+        valid = false;
+        document.getElementById('error').innerHTML = "invalid day.";
+    }
+
+    if(month < 1 || month > 12) {
+        valid = false;
+        document.getElementById('error').innerHTML = "invalid month.";
+    }
+
+    const currentYear = new Date().getFullYear();
+    if(year < 1900 || year > currentYear) {
+        valid = false;
+        document.getElementById('error').innerHTML = "invalid year.";
+    }
+
+    return valid;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+let button = document.querySelector("#submit");
+button.addEventListener('submit',calcAge);
